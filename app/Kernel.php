c57428dcfe2063d2dcaafbe03dd50d64b815e601
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-06-17 12:38:10 +0200

    Adds ENV Parser

--- app/Kernel.php
+++ app/Kernel.php
@@ -6,10 +6,30 @@ namespace App;
 
 class Kernel
 {
+    static $env;
+
     public function run(){
         echo 'WP Converter is ready.'."\n";
 
-        $src  = new DB('localhost',3306,'mirold','2m%Jwe59','mirDBold');
-        $dest = new DB('localhost',3306,'mirDBUsrd','#5mlJk23zFyl30~9','mirDBsad');
+        Self::$env = (new EnvParser("../.env"))->parse();
+
+        $src  = new DB(
+                env('SRC_DB_HOST'),
+                env('SRC_DB_NAME'),
+                env('SRC_DB_PORT'),
+                env('SRC_DB_USER'),
+                env('SRC_DB_PASS'),
+            );
+        $dest  = new DB(
+                env('DST_DB_HOST'),
+                env('DST_DB_NAME'),
+                env('DST_DB_PORT'),
+                env('DST_DB_USER'),
+                env('DST_DB_PASS'),
+            );
+    }
+
+    static function env($key, $default = null){
+        return Self::$env[$key] ?? $default;
     }
 }
