ddd42b825b24fd59415f0f57799cbf2f88bee96c
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-06-17 12:30:48 +0200

    Adds DB Engine

--- app/Kernel.php
+++ app/Kernel.php
@@ -7,6 +7,9 @@ namespace App;
 class Kernel
 {
     public function run(){
-        echo 'PHP Boilerplate is ready.'."\n";
+        echo 'WP Converter is ready.'."\n";
+
+        $src  = new DB('localhost',3306,'mirold','2m%Jwe59','mirDBold');
+        $dest = new DB('localhost',3306,'mirDBUsrd','#5mlJk23zFyl30~9','mirDBsad');
     }
 }
