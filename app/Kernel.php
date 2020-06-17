405201f75831225495eafffeabc6075ca3d1f235
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-06-17 12:52:10 +0200

    Repairs DB Connector

--- app/Kernel.php
+++ app/Kernel.php
@@ -14,18 +14,18 @@ class Kernel
         Kernel::$env = (new EnvParser(".env"))->parse();
 
         $src  = new DB(
-                Kernel::env('SRC_DB_HOST'),
-                Kernel::env('SRC_DB_NAME'),
-                Kernel::env('SRC_DB_PORT'),
-                Kernel::env('SRC_DB_USER'),
-                Kernel::env('SRC_DB_PASS')
+            Kernel::env('SRC_DB_HOST'),
+            Kernel::env('SRC_DB_PORT'),
+            Kernel::env('SRC_DB_USER'),
+            Kernel::env('SRC_DB_PASS'),
+            Kernel::env('SRC_DB_NAME')
             );
         $dest  = new DB(
-                Kernel::env('DST_DB_HOST'),
-                Kernel::env('DST_DB_NAME'),
-                Kernel::env('DST_DB_PORT'),
-                Kernel::env('DST_DB_USER'),
-                Kernel::env('DST_DB_PASS')
+            Kernel::env('DST_DB_HOST'),
+            Kernel::env('DST_DB_PORT'),
+            Kernel::env('DST_DB_USER'),
+            Kernel::env('DST_DB_PASS'),
+            Kernel::env('DST_DB_NAME')
             );
 
         $src->connect();
